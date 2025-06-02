df47033542b2f2e8c51b0159b48da1de781ae412
Tamamo <122244266+EritXiaodou@users.noreply.github.com>
2025-06-02 19:42:40 +0800

    Update apotheosis.js

--- server_scripts/mod_replace/apotheosis/apotheosis.js
+++ server_scripts/mod_replace/apotheosis/apotheosis.js
@@ -1,1 +1,11 @@
-
+ServerEvents.recipes(event => {
+    event.shaped('apotheosis:boss_summoner', [
+        `aba`,
+        `bcb`,
+        `aba`
+    ], {
+        a: `apotheosis:rare_material`,
+        b: `apotheosis:epic_material`,
+        c: `minecraft:egg`
+    }).id('apotheosis:boss_summoner')
+})

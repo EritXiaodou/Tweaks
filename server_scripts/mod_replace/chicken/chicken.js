3fb4de121d3a1f51560dae57fae1d449dde7d6ba
Tamamo <122244266+EritXiaodou@users.noreply.github.com>
2025-06-02 19:44:10 +0800

    Create chicken.js

---
+++ server_scripts/mod_replace/chicken/chicken.js
@@ -0,0 +1,10 @@
+ServerEvents.recipes(event => {
+    /*event.shaped(`minecraft:warden_spawn_egg`, [
+        `aaa`,
+        `aba`,
+        `aaa`
+    ], {
+        a: `minecraft:oak_log`,
+        b: `minecraft:egg`
+    }).id(`tamamo_the_tweaks:warden_spawn_egg`)*/
+})
